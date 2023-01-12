490f37447d95420a65f6ff7847f7490dfdf12aeb
Heikki savolainen <hsavo@Heikki-MacBook-Pro.local>
2023-01-12 14:35:25 +0200

    Initial commit

---
+++ Tehtava 1.py
@@ -0,0 +1,1 @@
+print("Moi Heikki!")
---
+++ Tehtava2.py
@@ -0,0 +1,11 @@
+print("huomenta!")
+#kommentti, muuttujan esittely ja arvon sijoittaminen muuttujaan
+# yleensä kommentteja mahdollisimman vähän ja koodi sen verran helppolukuista ettei niitä tarvitse
+#kotitehtävissä kuitenkin kommentit hyviä jne...
+name = "user"
+print(name)
+
+#syötteen lukeminen käyttäjältä
+name = input("Anna nimesi:")
+print("")
+print(name)
