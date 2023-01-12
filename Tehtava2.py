c47e36dbef7ef8a406c9cc570343fbea4e0fbf66
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-12 21:52:08 +0200

    6

--- Tehtava2.py
+++ Tehtava2.py
@@ -1,7 +1,7 @@
 print("testi1")
 name = input("What is your name?")
 print()
-message_f = f"Your name is {name} , am i right?"
+message_f = f"Your name is {name}, am i right?"
 print(message_f)
 print()
 print("Have a nice day!")
