c444b40ec38d317a5efa73156078ae22d0d4d3ab
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-14 19:02:42 +0200

    2 commit

--- Moduuli02/Tehtava01.py
+++ Moduuli02/Tehtava01.py
@@ -1,9 +1,13 @@
 """
 Kirjoita ohjelma, joka kysyy nimesi ja sen jälkeen tervehtii sinua omalla nimelläsi.
 """
-
+"""
 name = input("Mikä on nimesi?")
 print()
 print("Terve, " + str(name) + "!")
-
-#vastaavasti f-lauseketa käyttäen
+"""
+#vastaavasti f-lauseketta käyttäen
+print("")
+name = input("Mikä on nimesi?")
+print("")
+print(f"Terve, {name}!")
