f0ee28b35786e99e65de0f455f573aa2944a50bf
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-15 19:20:12 +0200

    1 commit

---
+++ Moduuli02/Tehtava03.py
@@ -0,0 +1,12 @@
+"""
+Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden.
+Ohjelma tulostaa suorakulmion piirin ja pinta-alan. Suorakulmion piiri tarkoittaa sen neljän sivun yhteispituutta.
+"""
+print("Anna suorakulmion kanta (cm):")
+kanta = float(input()
+print("Anna suorakulmion korkeus (cm):")
+korkeus = float(input)
+pinta-ala = int(kanta) * int(korkeus)
+piiri = int(kanta) * 2 + int(korkeus) * 2
+
+print("Suorakulmion pinta-ala on " + str(pinta-ala) " ja piiri on " + str(piiri) " !")
