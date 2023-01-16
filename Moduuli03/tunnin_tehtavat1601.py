3b4e1f3a2163a41da98c3e33a48c07ba415627c8
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-16 13:39:16 +0200

    16.1. / 2

---
+++ Moduuli03/tunnin_tehtavat1601.py
@@ -0,0 +1,9 @@
+#"tunnin tehtävät ja asiat 16.1.23 klo 13-16"
+#int = kokonaisluku
+#float = desimaaliluku
+# jakolaskuna pythonissa // palauttaa aina KOKONAISLUVUN! desimaalin sijaan (edellisen moduulin kilogrammatehtävä)
+# HUOM! yhtäsuuruusmerkki on == eli 2 x =
+
+rahat = float(input("Anna rahamäärä: "))
+if rahat>=5:
+    print("Voit ostaa latten.")
