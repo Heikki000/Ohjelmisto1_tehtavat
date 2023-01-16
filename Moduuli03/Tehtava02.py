0230e04be91cc7b81dcb4fd04fb4357973377046
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-16 14:25:59 +0200

    16.1. / 4

--- Moduuli03/Tehtava02.py
+++ Moduuli03/Tehtava02.py
@@ -10,3 +10,18 @@ C on ikkunaton hytti autokannen alapuolella.
 
 Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
 """
+luokka= input("Anna hyttiluokka: ")
+if (luokka == "LUX" or luokka == "lux"):
+    print("LUX on parvekkeellinen hytti yläkannella.")
+
+elif (luokka == "A" or luokka == "a"):
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+
+elif (luokka == "B" or luokka == "b"):
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+elif (luokka == "C" or luokka == "c"):
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka.")
