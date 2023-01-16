ba2119f5a43322c77749ce8e6941169a7807c012
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-16 13:52:17 +0200

    16.1. / 3

--- Moduuli03/Tehtava01.py
+++ Moduuli03/Tehtava01.py
@@ -1,5 +1,11 @@
 """
 Kirjoita ohjelma, joka kysyy kalastajalta kuhan pituuden senttimetreinä.
 Jos kuha on alamittainen, ohjelma käskee laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle,
-montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+montako senttiä alimmasta sallitusta pyyntimitasta puuttuu.
+Kuha on alamittainen, jos sen pituus on alle 37 cm.
 """
+pituus = float(input("Anna kuhan pituus:"))
+if pituus < 37:
+    print("Kuha on alamittainen")
+    ero = 37 - pituus
+    print(f"Laske kuha takaisin järveen, koska sallitusta pyyntimitasta puuttuu {ero}.")
--- Moduuli03/tunnin_tehtavat1601.py
+++ Moduuli03/tunnin_tehtavat1601.py
@@ -3,7 +3,13 @@
 #float = desimaaliluku
 # jakolaskuna pythonissa // palauttaa aina KOKONAISLUVUN! desimaalin sijaan (edellisen moduulin kilogrammatehtävä)
 # HUOM! yhtäsuuruusmerkki on == eli 2 x =
-
+"""
 rahat = float(input("Anna rahamäärä: "))
 if rahat>=5:
     print("Voit ostaa latten.")
+    """
+ika = int(input("Anna ikasi:"))
+if (ika >=18):
+    print("Olet siis taysi-ikainen.")
+    aika = ika - 18
+    print(f"Olet ollut sitä jo {aika} vuotta")
