96cb8f8a1429856bfe26699feb7656f41848ecfa
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-20 11:24:39 +0200

    20.1. / 4

--- Moduuli03/Tehtava04.py
+++ Moduuli03/Tehtava04.py
@@ -1,12 +1,13 @@
 """
 Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi.
-Vuosi on karkausvuosi, jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat karkausvuosia
-vain jos ne ovat jaollisia myös neljälläsadalla.
+Vuosi on karkausvuosi, jos se on jaollinen neljällä.
+Sadalla jaolliset vuodet ovat karkausvuosia
+VAIN jos ne ovat jaollisia myös neljälläsadalla.
 """
-Print:("Ohjelma kertoo, onko antamasi vuosiluku karkausvuosi.")
-vuosi = int(input("Anna vuosiluku"))
+print("Ohjelma kertoo, onko antamasi vuosiluku karkausvuosi.")
+vuosi = int(input("Anna vuosiluku: "))
 if(vuosi % 4 == 0):
     if(vuosi % 400 == 0 or vuosi % 100 != 0):
-        print("Antamasi vuosi on karkausvuosi")
+        print(f"Antamasi vuosi, {vuosi} on karkausvuosi!")
     else:
-        (print("Vuosi ei ole karkausvuosi!"))
+        print(f"Antamasi vuosi, {vuosi} ei ole karkausvuosi!")
