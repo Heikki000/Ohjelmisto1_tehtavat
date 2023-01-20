38ce4ff118b22378f46295f10f0b38065c1f4b51
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-20 15:06:32 +0200

    20.1. / 3

--- Moduuli04/Tehtava06.py
+++ Moduuli04/Tehtava06.py
@@ -16,17 +16,17 @@ riittää testata, toteuttaako piste epäyhtälön x^2+y^2<1.)
 """
 import random
 
-N = 100
+N = 10000
 counter = 0
 n = 0
 while counter < N:
-    counter += counter
+    counter += 1
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
     #print(f"arvottu piste {x}, {y}")
 
     #onko piste yksikköympyrän sisällä: (x^2+y^2<1)
-    print(f"x * x + y * y < 1")
+    #print(f"x * x + y * y < 1")
     if x * x + y * y < 1:
        # print("on sisällä")
         n+= 1
