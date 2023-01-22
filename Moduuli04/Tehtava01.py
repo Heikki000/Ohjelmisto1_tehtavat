fcda98df436ae0d2f51b20b9f5753865fbc0b6c4
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-22 13:50:05 +0200

    22.1. / 1

--- Moduuli04/Tehtava01.py
+++ Moduuli04/Tehtava01.py
@@ -1,3 +1,16 @@
 """
 Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
 """
+print("Ohjelma tulostaa kolmella jaolliset luvut väliltä 1..1000")
+print()
+times = 1000
+number = 0
+while number < times:
+    number += 1
+#   print(number) testaa, että ohjelma käy läpi kaikki halutut numerot
+    if number % 3 == 0:
+        print(number)
+print("Ohjelma valmis!")
+
+
+
