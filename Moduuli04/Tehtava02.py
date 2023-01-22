e7a2058dd3a4815d9ef4c6a8336eedebf485c34e
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-22 19:09:16 +0200

    22.1. / 5

--- Moduuli04/Tehtava02.py
+++ Moduuli04/Tehtava02.py
@@ -3,7 +3,7 @@ Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttä
 Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
 """
 
-tuuma = float(input("Anna tuumien määrä niin muutan sen sinulle senttimetreiksi:"))
+tuuma = float(input("Anna tuumien määrä niin muutan sen sinulle senttimetreiksi: "))
 
 while tuuma > 0:
     print(f"Antamasi luku on senttimetreinä {tuuma * 2.54} senttimetriä. ")
--- Moduuli04/Tehtava03.py
+++ Moduuli04/Tehtava03.py
@@ -2,11 +2,13 @@
 Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
-luku = float(input("Anna lukuja ja paina lopuksi 'ENTER' niin kerron, mitkä luvut olivat suurin ja pienin:"))
-while luku != '3':
-    luku = (input("Anna luku: "))
-else:
-    print(f"Suurin antamasi luku oli {max(luku)} ja pienin luku oli {min(luku)} !")
+print("Anna lukuja ja paina lopuksi ENTER niin kerron, mitkä luvut olivat suurin ja pienin.")
+luku = (input("Anna luku: "))
+while luku == float:
+    if luku == float(input("Anna luku: "))
+        luku = float(input("Anna luku: "))
 
-print()
+print(f"Suurin antamasi luku oli {max(luku)} ja pienin luku oli {min(luku)} !")
+
+    print()
 print("Ohjelma loppu!")
