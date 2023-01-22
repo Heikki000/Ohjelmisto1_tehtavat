d2cb808a4a3ec5b8468924754bf741ecb7e0b83d
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-22 15:57:59 +0200

    22.1. / 3

--- Moduuli04/Tehtava03.py
+++ Moduuli04/Tehtava03.py
@@ -2,3 +2,11 @@
 Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
+luku = float(input("Anna lukuja ja paina lopuksi 'ENTER' niin kerron, mitkä luvut olivat suurin ja pienin:"))
+while luku != '3':
+    luku = (input("Anna luku: "))
+else:
+    print(f"Suurin antamasi luku oli {max(luku)} ja pienin luku oli {min(luku)} !")
+
+print()
+print("Ohjelma loppu!")
