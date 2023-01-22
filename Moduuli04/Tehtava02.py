d3e264e859df3166bbcea057ff105ecf1e094a00
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-22 14:41:22 +0200

    22.1. / 2

--- Moduuli04/Tehtava02.py
+++ Moduuli04/Tehtava02.py
@@ -2,3 +2,14 @@
 Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
 Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
 """
+
+tuuma = float(input("Anna tuumien määrä niin muutan sen sinulle senttimetreiksi:"))
+
+while tuuma > 0:
+    print(f"Antamasi luku on senttimetreinä {tuuma * 2.54} senttimetriä. ")
+    tuuma = float(input("Anna tuumien määrä: "))
+else:
+    print()
+    print("Taisit antaa virheellisen luvun.")
+
+print("Ohjelma loppu.")
