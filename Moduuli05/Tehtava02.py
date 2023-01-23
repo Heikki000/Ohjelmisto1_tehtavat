50facbd7914f2054c4465c31b44be3fd5d5a09ed
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-23 18:14:08 +0200

    23.1. / 3

--- Moduuli05/Tehtava02.py
+++ Moduuli05/Tehtava02.py
@@ -5,3 +5,13 @@ Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksess
 Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla
 sort-metodille argumentiksi reverse=True.
 """
+print("Anna numeroita ja lopuksi paina ENTER, niin saat listan 5 suurimmasta luvusta suurimmasta alkaen: ")
+luvut = []
+luku = input("Anna ensimmäinen luku: ")
+while luku != "":
+    luvut.append (int(luku))
+    luku = input("Anna seuraava luku tai lopeta painamalla ENTER: ")
+
+luvut.sort(reverse=True)
+
+print(f"{luvut[0:5]}")
