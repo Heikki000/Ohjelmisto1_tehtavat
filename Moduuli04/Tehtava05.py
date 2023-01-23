20f598d4c79e6284887e338d3c1df64f6639260b
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-23 14:28:25 +0200

    23.1. / 4

--- Moduuli04/Tehtava05.py
+++ Moduuli04/Tehtava05.py
@@ -3,19 +3,32 @@ Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan.
 Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen.
 Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. (Oikea käyttäjätunnus on python ja salasana rules).
-"""
-tunnus = python
-salasana = rules
+
+tunnus = str(python)
+salasana = str(rules)
 print("Anna käyttäjätunnus ja salasana:")
 tunnus1 = input("käyttäjätunnus: ")
-while tunnus1 != "python":
-    salasana1 = input(str("salasana: "))
-    if salasana1 = "rules":
+while tunnus1 != tunnus:
+    salasana1 = input("salasana: ")
+    if salasana1 == salasana:
         tunnus1 = input(str("Väärin, anna käyttäjätunnus:"))
-    elif salasana1 != "rules":
+    elif salasana1 != salasana:
         tunnus1 = input("Väärin, anna käyttäjätunnus: ")
 else:
     salasana1 = input("salasana: ")
-    if salasana1 = "rules":
+    if salasana1 == salasana:
         print("Tervetuloa!")
+"""
+kerta = 0
+while kerta < 5:
+    tunnus = input("Anna käyttäjä: ")
+    salasana = input("Anna salasana: ")
+    if tunnus == 'python' and salasana == 'rules':
+        print("Tervetuloa!")
+        kerta = 5
+    else:
+        print("Pääsy evätty! Yritä uudelleen!")
+        kerta += 1
+        if kerta == 5:
+            print("Pääsy evätty, liian monta yritystä!")
 
