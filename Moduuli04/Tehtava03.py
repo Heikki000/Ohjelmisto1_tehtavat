f357fd1a27ebdac48c1beb790288aaced2848fe2
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-23 12:55:26 +0200

    23.1. / 1

--- Moduuli04/Tehtava03.py
+++ Moduuli04/Tehtava03.py
@@ -4,11 +4,39 @@ Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
 print("Anna lukuja ja paina lopuksi ENTER niin kerron, mitkä luvut olivat suurin ja pienin.")
 luku = (input("Anna luku: "))
-while luku == float:
-    if luku == float(input("Anna luku: "))
-        luku = float(input("Anna luku: "))
+iso = int(luku)
+pieni = int(luku)
 
-print(f"Suurin antamasi luku oli {max(luku)} ja pienin luku oli {min(luku)} !")
+while luku != "":
+    luku = input("Anna luku: ")
+    if luku != "":
+        luku = int(luku)
+        if luku > iso:
+            iso = luku
+        elif luku < pieni:
+            pieni = luku
+"""
+while luku != "":            
+    luku = input("Anna luku: ")
+    luku = int(luku)
+    if luku > iso:
+        iso = luku
+    elif luku < pieni:
+        pieni = luku
+else:
+    print(f"Suurin antamasi luku oli {iso} ja pienin luku oli {pieni} !")
+"""
+
+print(f"Suurin antamasi luku oli {iso} ja pienin luku oli {pieni} !")
+
+print("Ohjelma loppu!")
+
+"""
+while luku == float:
+    luku = float(input("Anna luku: "))
+while luku == "":
+    print(f"Suurin antamasi luku oli {max(luku)} ja pienin luku oli {min(luku)} !")
 
     print()
 print("Ohjelma loppu!")
+"""
