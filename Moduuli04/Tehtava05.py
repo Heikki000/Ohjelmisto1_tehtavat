fb4c0d3808d3cc89c7922c8022346b6c28026447
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-23 13:03:14 +0200

    23.1. / 2

--- Moduuli04/Tehtava05.py
+++ Moduuli04/Tehtava05.py
@@ -4,3 +4,18 @@ Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelle
 Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. (Oikea käyttäjätunnus on python ja salasana rules).
 """
+tunnus = python
+salasana = rules
+print("Anna käyttäjätunnus ja salasana:")
+tunnus1 = input("käyttäjätunnus: ")
+while tunnus1 != "python":
+    salasana1 = input(str("salasana: "))
+    if salasana1 = "rules":
+        tunnus1 = input(str("Väärin, anna käyttäjätunnus:"))
+    elif salasana1 != "rules":
+        tunnus1 = input("Väärin, anna käyttäjätunnus: ")
+else:
+    salasana1 = input("salasana: ")
+    if salasana1 = "rules":
+        print("Tervetuloa!")
+
