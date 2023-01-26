4cd093d58c48811b9905a1b06c11a56d3c97c7bf
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-26 18:21:04 +0200

    26.1. / 7

--- Moduuli05/Tehtava04.py
+++ Moduuli05/Tehtava04.py
@@ -7,11 +7,14 @@ kuin ne syötettiin. käytä for-toistorakennetta nimien kysymiseen ja for/in to
 print("Anna 5 kaupungin nimet yksi kerrallaan niin tulostan ne allekkain antamassasi järjestysksessä.\n")
 
 kaupungit = []
-enter1 = 0
+enter1 = 1
 kaupunki = input("Anna kaupunki: ")
-while enter1 < 4:
-    kaupungit.append(kaupunki)
-    kaupunki = input("Anna seuraava kaupunki: ")
-    enter1 += 1
+kaupungit.append(kaupunki)
 
-print(kaupungit)
+while enter1 < 5:
+    enter1 += 1
+    kaupunki = input("Anna seuraava kaupunki: ")
+    kaupungit.append(kaupunki)
+
+for kaupunki in kaupungit:
+    print(f"{kaupunki}")
