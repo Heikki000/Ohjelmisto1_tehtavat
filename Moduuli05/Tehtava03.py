10adcdd21a25102278f99e74de4657e14e5941ec
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-26 15:23:31 +0200

    26.1. / 3

--- Moduuli05/Tehtava03.py
+++ Moduuli05/Tehtava03.py
@@ -5,3 +5,11 @@ Tässä tehtävässä alkulukuja ovat luvut, jotka ovat jaollisia vain ykkösell
 Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
 Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
 """
+# % 0 jakojäännös
+number = int(input("Anna luku:"))
+
+for iterator in range(2, number):
+    print(f"testataan lukua jakamalla se arvolla {iterator}")
+    print(f"jakojäännös: {number % iterator}")
+    if number % iterator == 0:
+        print(f"{number} ei ole alkuluku.")
