7ab6d4b5d221566c597f98fc442c6e416643b360
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-26 13:35:26 +0200

    26.1. / 1

---
+++ Moduuli06/Tunnin_tehtavat2601.py
@@ -0,0 +1,12 @@
+# huom tentti ap 9.2. (harjoitustenttimahdollisuus 2.2.)
+
+
+def say_hello(to_whom):
+    print(f"Terve {to_whom}!")
+    print(f"ja heippa, {to_whom}")
+    return "valmista tuli"
+
+
+result = say_hello("Kalle")
+print(result)
+print(say_hello("Viivi"))
