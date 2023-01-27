843e13c2744e20cbfb38d0ccfa10d1987adf09bc
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-27 11:11:11 +0200

    27.1. / 2

--- Moduuli05/Tehtava01.py
+++ Moduuli05/Tehtava01.py
@@ -1,8 +1,8 @@
 """Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
 Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
 Käytä for-toistorakennetta."""
-print()
-print("Ohjelma heittää niin monta arpakuutioita kun haluat!\n")
+
+print("\nOhjelma heittää niin monta arpakuutioita kun haluat!\n")
 
 
 import random
@@ -11,10 +11,10 @@ lukumäärä = int(input("Anna kuutioiden määrä: "))
 
 for index in range(lukumäärä):
     noppa = random.randint(1, 6)
-    print(f"Nopan silmäluku on {noppa}.")
+    # print(f"Nopan silmäluku on {noppa}.")
     summa = summa + noppa
 
-print(f"\nNoppien summa on {summa}!")
+print(f"\nNoppien silmälukujen summa on {summa}!")
 
 """
 print()
