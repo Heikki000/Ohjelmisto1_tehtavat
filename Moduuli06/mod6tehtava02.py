170b18643b643a1e55170d3615bb0c445832c3eb
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-27 13:50:39 +0200

    27.1. / 3

--- Moduuli06/mod6tehtava02.py
+++ Moduuli06/mod6tehtava02.py
@@ -4,3 +4,18 @@ Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa
 Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
 joka kysytään käyttäjältä ohjelman suorituksen alussa.
 """
+
+import random
+number_of_sides = int(input("Anna nopan tahkojen määrä: "))
+def roll_die(number_of_sides):
+    return random.randint(1, number_of_sides)
+
+#roll_die(number_of_sides)
+
+def play_game():
+    result = 0
+    while result != number_of_sides:
+        result = roll_die(number_of_sides)
+        print(result)
+
+play_game()
