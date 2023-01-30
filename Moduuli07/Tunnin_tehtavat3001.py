93a4a0bfd7077cb92575ddb43ae928cc7f6d39e8
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-30 15:11:26 +0200

    30.01 / 3

---
+++ Moduuli07/Tunnin_tehtavat3001.py
@@ -0,0 +1,15 @@
+#tehtävien läpikäynnistä:
+
+pikakoodi kirjoitettuna 2 erillisellä rivillä
+luvut.sort(reverse=True)
+luvut.sort()
+luvut.reverse()
+
+lyhennelmä pitemmin:
+if alkuluku:
+if alkuluku == True
+
+True, False asia olisi hyvä varuiksi kerrata
+
+Toisin kuin listassa tai monikossa, sama alkio voi esiintyä joukossa VAIN 1 KERRAN!!!!
+ERI TIETORAKENTEILLA ON ERI NIMISIÄ KOMENTOJA!!! Kuten esim add vs append!
