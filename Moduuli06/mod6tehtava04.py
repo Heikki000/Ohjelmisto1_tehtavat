da5a538049dae640642844bc77efbd2447795e08
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-30 11:06:47 +0200

    30.1. / 1

--- Moduuli06/mod6tehtava04.py
+++ Moduuli06/mod6tehtava04.py
@@ -6,8 +6,9 @@ jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
 
 def summa(numerolista):
     for n in numerolista:
-        print(sum(numerolista))
-        return
+        #print(sum(numerolista))
+        return (sum(numerolista))
 
 lista = [1, 2, 3, 4, 5]
+summa(lista)
 print(summa(lista))
--- Moduuli06/mod6tehtava05.py
+++ Moduuli06/mod6tehtava05.py
@@ -5,7 +5,16 @@ lista paitsi että siitä on karsittu pois kaikki parittomat luvut.
 Kirjoita testausta varten pääohjelma, jossa luot listan,
 kutsut funktiota ja tulostat sen jälkeen sekä alkuperäisen että karsitun listan.
 """
+def ei_parittomia(objekti_lista):
+    muokattu_lista = []
+    for luku in objekti_lista:
+        if luku % 2 == 0:
+            muokattu_lista.append(luku)
+    return muokattu_lista
 
-play_game()
 
-testi_lista = [1, 23, 44, 51, 90, 100, 101]
+testi_lista = [1, 23, 45, 67, 100, 200]
+print("Alkuperäinen lista on " + str(testi_lista) + ".")
+#print(testi_lista)
+print("\nMuokattu lista ilman parittomia lukuja on " + str(ei_parittomia(testi_lista)) + ".")
+#print(testi_lista)
