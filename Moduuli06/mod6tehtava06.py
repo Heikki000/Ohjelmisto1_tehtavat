a601222d9ce1f259df295440ba4de370a44cb1ed
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-01-30 11:27:26 +0200

    30.1. / 2

--- Moduuli06/mod6tehtava06.py
+++ Moduuli06/mod6tehtava06.py
@@ -5,3 +5,15 @@ Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilm
 kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
 Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
 """
+import math
+#ympyrän pinta-alan kaava on pi * r^2
+# säde, r on halkaisija, d / 2 -> pinta-ala halkaisijan avulla on pi/4 * d^2
+
+def yks_hinta(hinta, halkaisija):
+    neliohinta = hinta / (math.pi/4*halkaisija)
+    return neliohinta
+
+hinta = float(input("Anna pitsan myyntihinta:"))
+halkaisija = float(input("Anna pitsan halkaisija (cm):"))
+print(yks_hinta(hinta, halkaisija))
+
