fb8f75213c9edca41ca37b0c58ba7539f0b973b9
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-02-02 15:15:48 +0200

    01.02 / 3 joku error vielä

--- Moduuli08/tunnin-tehtavat010223.py
+++ Moduuli08/tunnin-tehtavat010223.py
@@ -5,8 +5,20 @@ import mysql.connector
 #Funktio tarvitsee parametrina pelaajan nimen.
 #Funktio ei palauta mitään.
 def pelaajan_paikka(nimi):
-    print("Ei vielä ihan valmis")
+    sql = "SELECT screen_name, location " + \
+          "FROM game" +\
+          "WHERE screen_name = '" + nimi + "'"
+
+    print(sql)  # tulostetaan lopullinen sql-lause
+    kursori = yhteys.cursor()  # kursorin avulla välitetään tietoa python-koodin
+    # tietokannan välillä
+    kursori.execute(sql)  # suoritetaan sql-lause
+    tulos = kursori.fetchall()  # haetaan haun tulos ja sijoitetaan se muuttujaan tulos
+    if kursori.rowcount >= 1:  # saatiinko dataa?
+        for rivi in tulos:
+            print(f"Nimi: {rivi[0]} on paikassa: {rivi[1]}")
     return
+
 
 #Funktio hakee ja tulostaa kaikkien pelaajien nimet ja paikat.
 #Funktio ei tarvitse parametreja eikä palauta mitään.
@@ -14,7 +26,16 @@ def pelaajan_paikka(nimi):
 def tulosta_pelaajat():
     sql = "SELECT screen_name, location " +\
         "FROM game"
-    print(sql)
+    print(sql)                  #tulostetaan lopullinen sql-lause
+    kursori = yhteys.cursor()   #kursorin avulla välitetään tietoa python-koodin
+                                #tietokannan välillä
+    kursori.execute(sql)        #suoritetaan sql-lause
+    tulos = kursori.fetchall()  #haetaan haun tulos ja sijoitetaan se muuttujaan tulos
+    if kursori.rowcount >= 1:   #saatiinko dataa?
+        for rivi in tulos:
+            print(f"Nimi: {rivi[0]} on paikassa: {rivi[1]}")
+    return
+
 
 
 #pääohjelma alkaa
@@ -27,7 +48,12 @@ yhteys = mysql.connector.connect(
          password='sala1',
          autocommit=True    #muutokset tietokantaan tehdään heti
          )
+#kutsutaan funktiota, joka tulostaa pelaajien nimet ja paikat
+tulosta_pelaajat()
 
+#funktio tulostaa halutun pelaajan paikan
+pelaaja = input("Anna pelaajan nimi: ")
+pelaajan_paikka(pelaaja)
 #suljetaan yhteys
 yhteys.close()
 
