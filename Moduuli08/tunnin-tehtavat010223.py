156db8fb64e57cd268edeec0a4fda5d18c31cc2d
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-02-02 14:24:08 +0200

    01.02 / 2

--- Moduuli08/tunnin-tehtavat010223.py
+++ Moduuli08/tunnin-tehtavat010223.py
@@ -1,13 +1,33 @@
 #ladataan tarvittava moduuli
 import mysql.connector
 
+#Funktio hakee ja tulostaa pelaajan nimen ja paikan
+#Funktio tarvitsee parametrina pelaajan nimen.
+#Funktio ei palauta mitään.
+def pelaajan_paikka(nimi):
+    print("Ei vielä ihan valmis")
+    return
+
+#Funktio hakee ja tulostaa kaikkien pelaajien nimet ja paikat.
+#Funktio ei tarvitse parametreja eikä palauta mitään.
+#Funktio käyttää pääohjelmassa luotua tietokantayhteyttä.
+def tulosta_pelaajat():
+    sql = "SELECT screen_name, location " +\
+        "FROM game"
+    print(sql)
+
+
+#pääohjelma alkaa
 #muodostetaan yhteys tietokantaan
 yhteys = mysql.connector.connect(
          host='127.0.0.1', # on sama asia kuin localhost
-         port= 3306,
+         port= 3306,    #tietyille asioille on omat porttinsa, esim. näppäimistö, internet
          database='flight_game',
          user='user1',
          password='sala1',
          autocommit=True    #muutokset tietokantaan tehdään heti
          )
+
+#suljetaan yhteys
+yhteys.close()
 
