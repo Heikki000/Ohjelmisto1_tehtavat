53256ce0a7dd227f1480bf63e54fbc22437c600b
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-02-06 10:16:51 +0200

    6.2. / 1 valmis

--- Moduuli08/mod8teht01.py
+++ Moduuli08/mod8teht01.py
@@ -5,21 +5,28 @@ lentokenttätietokannasta. ICAO-koodi on tallennettuna airport-taulun ident-sara
 """
 import mysql.connector
 
-
-
 def connect_db():
     return mysql.connector.connect(
          host='127.0.0.1',
-         port= 3306,
+         port=3306,
          database='flight_game',
          user='user1',
          password='sala1',
          autocommit=True
          )
-connect = connect_db()
+connection = connect_db()
 #print connect
+icao = input("Anna haettavan kentän ICAO: ")
 
-def get_airport():
-    query = "select name, sumicipality from airport where ident ='efhk'"
+def get_airport(icao):
+    query = f"select name, municipality from airport where ident ='{icao}'"
     cursor = connection.cursor()
     cursor.execute(query)
+    result = cursor.fetchone()  #fetchall tuo kaikki monikkona... [] on lista ja () on dictionary tai setti
+    if cursor.rowcount > 0:
+        return f"Kentän nimi: {result[0]}, paikkakunta: {result[1]}"
+    else:
+        return "Ei tuloksia."
+
+result = get_airport(icao)
+print(result)
