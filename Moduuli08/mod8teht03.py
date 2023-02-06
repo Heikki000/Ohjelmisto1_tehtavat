b8137883dc4983e97de012ae972d94e1508c9e37
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-02-06 11:48:41 +0200

    6.2. / 3

---
+++ Moduuli08/mod8teht03.py
@@ -0,0 +1,36 @@
+"""
+Kirjoita ohjelma, joka kysyy käyttäjältä kahden lentokentän ICAO-koodit.
+Ohjelma ilmoittaa lentokenttien välisen etäisyyden kilometreinä. Laskenta perustuu tietokannasta haettuihin
+koordinaatteihin. Laske etäisyys geopy-kirjaston avulla: https://geopy.readthedocs.io/en/stable/.
+Asenna kirjasto valitsemalla View / Tool Windows / Python Packages. Kirjoita hakukenttään geopy ja vie asennus loppuun.
+"""
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port=3306,
+         database='flight_game',
+         user='user1',
+         password='sala1',
+         autocommit=True
+         )
+
+def search_ICAO(input1, input2):
+    sql_command = "select airport.latitude_deg, airport.longitude_deg from airport where ident = '" + input1 + "' or ident = '" + input2 + "'"
+    airport = ()
+    cursor = connection.cursor()
+    cursor.execute(sql_command)
+    total = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in total:
+            airport += row[0], row[1]
+    return airport
+
+
+
+while True:
+    ICAO1, ICAO2 = input("Anna 1. kentän ICAO: "), input("Anna 2. kentän ICAO: ")
+    cordinates = search_ICAO(ICAO1, ICAO2)
+    place1 = cordinates[0], cordinates[1]
+    place2 = cordinates[2], cordinates[3]
+    print(f"Lentokenttien etäisyys on: {geopy.distance.distance(place1, place2).km:1.2f}km")
