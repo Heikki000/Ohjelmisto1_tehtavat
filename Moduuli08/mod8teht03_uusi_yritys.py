779a8904ca774828257ee79b97fe4822e54cac0f
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-03-05 23:22:20 +0200

    5.3. sain toimimaan molemmat eri versiot

--- Moduuli08/mod8teht03_uusi_yritys.py
+++ Moduuli08/mod8teht03_uusi_yritys.py
@@ -5,6 +5,7 @@ koordinaatteihin. Laske etäisyys geopy-kirjaston avulla: https://geopy.readthed
 Asenna kirjasto valitsemalla View / Tool Windows / Python Packages. Kirjoita hakukenttään geopy ja vie asennus loppuun.
 """
 import mysql.connector
+import math
 
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
@@ -27,13 +28,24 @@ def search_ICAO(input1, input2):
             airport += row[0], row[1]
     return airport
 
+def distance(lat1, lon1, lat2, lon2):
+    r = 6371  # Radius of the Earth in kilometers
 
+        # Convert latitude and longitude to radians
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+        # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    matka = r * c
+    return matka
 
 while True:
     ICAO1, ICAO2 = input("Anna 1. kentän ICAO: "), input("Anna 2. kentän ICAO: ")
     cordinates = search_ICAO(ICAO1, ICAO2)
-    print(cordinates)
-    place1 = cordinates[0], cordinates[1]
-    place2 = cordinates[2], cordinates[3]
-    from geopy import distance
-    print(f"Lentokenttien etäisyys on: {distance.distance(place1,place2).km:1.2f}km")
+    distance(cordinates[0], cordinates[1], cordinates[2], cordinates[3])
+    print(distance(cordinates[0], cordinates[1], cordinates[2], cordinates[3]))
+    etäisyys = distance(cordinates[0], cordinates[1], cordinates[2], cordinates[3])
+    print(f"Lentokenttien etäisyys on: {etäisyys:4.2f} km")
